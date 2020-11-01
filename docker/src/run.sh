1e2da150ed73e0a9b32dc51d060bf69d6ea66cfc
Daniel Bahrdt <daniel.git@funroll-loops.de>
2020-11-02 00:14:12 +0100

    [docker] add release build

--- docker/src/run.sh
+++ docker/src/run.sh
@@ -4,6 +4,7 @@ export OMP_THREAD_LIMIT=${PATH_FINDER_NUM_THREADS:-4}
 export OMP_NUM_THREADS=${PATH_FINDER_NUM_THREADS:-4}
 EXECUTABLE_SANITIZED=${SRC_DIR}/PathFinderCli/build/file_creator
 EXECUTABLE_ULTRA=${SRC_DIR}/PathFinderCli/build_ultra/file_creator
+EXECUTABLE_RELEASE=${SRC_DIR}/PathFinderCli/build_release/file_creator
 
 if [ "$1" = "gdb" ]; then
     echo "Executing cgdb"
@@ -17,6 +18,9 @@ elif [ "$1" = "ultra" ]; then
 elif [ "$1" = "asan" ]; then
     echo "Executing with asan"
     ${EXECUTABLE_SANITIZED} -f /graph.txt -l 10 -o /home/builder/out
+elif [ "$1" = "release" ]; then
+    echo "Executing with release"
+    ${EXECUTABLE_RELEASE} -f /graph.txt -l 10 -o /home/builder/out
 else
     echo "Invalid command ${i}."
     echo "Valid commands are gdb, valgrind, ultra, asan"
